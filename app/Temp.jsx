84502add6607ba4c9cb889d16b0cb08226635a81
trevjnels <trevorjohnnels@gmail.com>
2019-09-08 16:33:09 -0600

    attempting to connect RDS

--- app/Temp.jsx
+++ app/Temp.jsx
@@ -19,18 +19,18 @@ export class Temp extends React.Component {
 		super(props);
 
 		this.state = {
-			BCC : {
-				currentTemp : 20,
-				maxTemp     : 30,
-				minTemp     : 13,
-				visibility  : 16093,
-				humidity    : 60,
-				wind        : { speed: 10.3, deg: 180, gust: 13.4 },
-				rain        : { '1h': 0.25 },
-				description : 'Rain',
-				sunrise     : 1567947629,
-				sunset      : 1567993707
-			},
+			// BCC : {
+			// 	currentTemp : 20,
+			// 	maxTemp     : 30,
+			// 	minTemp     : 13,
+			// 	visibility  : 16093,
+			// 	humidity    : 60,
+			// 	wind        : { speed: 10.3, deg: 180, gust: 13.4 },
+			// 	rain        : { '1h': 0.25 },
+			// 	description : 'Rain',
+			// 	sunrise     : 1567947629,
+			// 	sunset      : 1567993707
+			// },
 			LCC : {
 				currentTemp : 21,
 				maxTemp     : 29,
@@ -39,43 +39,45 @@ export class Temp extends React.Component {
 				humidity    : 61,
 				wind        : { speed: 12.3, deg: 181, gust: 15.4 },
 				rain        : { '1h': 0.35 },
-				description : 'Rain'
+				description : 'Rain',
+				sunrise     : 1567947629,
+				sunset      : 1567993707
 			}
 		};
 		this.weatherUpdate = this.weatherUpdate.bind(this);
 	}
 	weatherUpdate() {
-		axios
-			.get(`http://avyoats.com:3000/weather/BCC`)
-			// .then((data) => JSON.parse(data))
-			.then((data) => {
-				console.log(data['data']);
-				var stringed = data['data'];
-				var main = stringed['main'];
+		// axios
+		// 	.get(`http://avyoats.com:3000/weather/BCC`)
+		// 	// .then((data) => JSON.parse(data))
+		// 	.then((data) => {
+		// 		console.log(data['data']);
+		// 		var stringed = data['data'];
+		// 		var main = stringed['main'];
 
-				this.setState({
-					BCC : {
-						currentTemp : main['temp'],
-						maxTemp     : main['temp_max'],
-						minTemp     : main['temp_min'],
-						visibility  : stringed['visability'],
-						humidity    : main['humidity'],
-						wind        : {
-							speed : stringed['wind']['speed'],
-							deg   : stringed['wind']['deg'],
-							gust  : stringed['wind']['gust']
-						},
-						rain        : { '1h': stringed['rain']['1hr'] },
-						description : stringed['weather'][0]['main'],
-						sunset      : stringed['sys']['sunset'],
-						sunrise     : stringed['sys']['sunrise']
-					}
-				});
-				console.log('$$$$$', this.state['BCC']);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
+		// 		this.setState({
+		// 			BCC : {
+		// 				currentTemp : main['temp'],
+		// 				maxTemp     : main['temp_max'],
+		// 				minTemp     : main['temp_min'],
+		// 				visibility  : stringed['visability'],
+		// 				humidity    : main['humidity'],
+		// 				wind        : {
+		// 					speed : stringed['wind']['speed'],
+		// 					deg   : stringed['wind']['deg'],
+		// 					gust  : stringed['wind']['gust']
+		// 				},
+		// 				rain        : { '1h': stringed['rain']['1hr'] },
+		// 				description : stringed['weather'][0]['main'],
+		// 				sunset      : stringed['sys']['sunset'],
+		// 				sunrise     : stringed['sys']['sunrise']
+		// 			}
+		// 		});
+		// 		console.log('$$$$$', this.state['BCC']);
+		// 	})
+		// 	.catch((err) => {
+		// 		console.log(err);
+		// 	});
 		axios
 			.get(`http://avyoats.com:3000/weather/LCC`)
 			// .then((data) => JSON.parse(data))
@@ -97,7 +99,9 @@ export class Temp extends React.Component {
 							gust  : stringed['wind']['gust']
 						},
 						rain        : { '1h': stringed['rain']['1hr'] },
-						description : stringed['weather'][0]['main']
+						description : stringed['weather'][0]['main'],
+						sunset      : stringed['sys']['sunset'],
+						sunrise     : stringed['sys']['sunrise']
 					}
 				});
 				console.log('$$$$$', this.state['BCC']);
@@ -117,43 +121,42 @@ export class Temp extends React.Component {
 
 	render() {
 		var temp;
-		if (this.props.site === 'BCC') {
-			temp = tempParser(this.state['BCC'].currentTemp);
-			return (
+		// if (this.props.site === 'BCC') {
+		// 	temp = tempParser(this.state['BCC'].currentTemp);
+		// 	return (
+		// 		<div>
+		// 			<h2>Brighton Weather (BCC)</h2>
+		// 			<div>
+		// 				<div>
+		// 					<span>{temp} degress f</span>
+		// 				</div>
+		// 				{/* <div>
+		// 					Mid: <span>{this.state['BCC'].mid} degress</span>
+		// 				</div>
+		// 				<div>
+		// 					Top: <span>{this.state['BCC'].top} degress</span>
+		// 				</div> */}
+		// 			</div>
+		// 		</div>
+		// 	);
+		// } else {
+		temp = tempParser(this.state['LCC'].currentTemp);
+		return (
+			<div>
+				<h2>Alta Weather (LCC)</h2>
 				<div>
-					<h2>Brighton Weather (BCC)</h2>
 					<div>
-						<div>
-							<span>{temp} degress f</span>
-						</div>
-						{/* <div>
-							Mid: <span>{this.state['BCC'].mid} degress</span>
-						</div>
-						<div>
-							Top: <span>{this.state['BCC'].top} degress</span>
-						</div> */}
+						<span>{temp} degress f</span>
 					</div>
-				</div>
-			);
-		} else {
-			temp = tempParser(this.state['LCC'].currentTemp);
-			return (
-				<div>
-					<h2>Alta Weather (LCC)</h2>
-					<div>
-						<div>
-							<span>{temp} degress f</span>
-						</div>
-						{/* <div>
+					{/* <div>
 							Mid: <span>{this.state['LCC'].mid} degress</span>
 						</div>
 						<div>
 							Top: <span>{this.state['LCC'].top} degress</span>
 						</div> */}
-					</div>
 				</div>
-			);
-		}
+			</div>
+		);
 	}
 }
 
--- backend/db.js
+++ backend/db.js
@@ -1,30 +1,31 @@
 const pg = require('pg');
 const { pgUser, pgPassword, awsEndpoint } = require('../keys/privateKeys.js');
 
-// const pool = new Pool({
-// 	user     : pgUser,
-// 	password : pgPassword,
-// 	host     : awsEndpoint,
-// 	database : 'avyreports',
-// 	port     : 5432
-// });
-var dbConfig = {
+const pool = new pg.Pool({
 	user     : pgUser,
 	password : pgPassword,
 	host     : awsEndpoint,
 	database : 'avyreports',
 	port     : 5432
-};
-
-var client = new pg.Client(dbConfig);
-client.connect((err) => {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log('yay');
-	}
 });
-client.end(); // pool.connect((err) => {
+// var dbConfig = {
+// 	user     : pgUser,
+// 	password : pgPassword,
+// 	host     : awsEndpoint,
+// 	database : 'avyreports',
+// 	port     : 5432
+// };
+
+// var client = new pg.Client(dbConfig);
+// client.connect((err) => {
+// 	if (err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log('yay');
+// 	}
+// });
+// client.end()
+// pool.connect((err) => {
 // 	if (err) {
 // 		console.log(err);
 // 	} else {
@@ -34,12 +35,12 @@ client.end(); // pool.connect((err) => {
 var createTableString =
 	'CREATE TABLE report(id serial PRIMARY KEY, date_issued VARCHAR (300) NOT NULL, current_conditions VARCHAR (1000) NOT NULL, avalanche_problem_1 VARCHAR (100) NOT NULL, avalanche_problem_2 VARCHAR (100) NOT NULL, avalanche_problem_3 VARCHAR (100) NOT NULL, avalanche_problem_1_description VARCHAR (2000) NOT NULL, avalanche_problem_2_description VARCHAR (2000) NOT NULL, avalanche_problem_3_description VARCHAR (2000) NOT NULL, bottom_line VARCHAR (500) NOT NULL, overall_danger_rose_image VARCHAR (100) NOT NULL, overall_danger_rating VARCHAR (50) NOT NULL, created_on TIMESTAMP NOT NULL)';
 
-// pool.query(createTableString, (err, resp) => {
-// 	if (err) console.log('~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ error in initailize reports'), console.log(err);
-// 	else {
-// 		console.log('sucessfully loaded into DB');
-// 	}
-// });
+pool.query(createTableString, (err, resp) => {
+	if (err) console.log('~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ error in initailize reports'), console.log(err);
+	else {
+		console.log('sucessfully loaded into DB');
+	}
+});
 // date_issued
 // current_conditions
 // avalanche_problem_1
