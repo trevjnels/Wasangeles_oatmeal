38eb77efbacac916794bf9de9c6f4e05836c4f1a
trevjnels <trevorjohnnels@gmail.com>
2019-09-08 16:39:57 -0600

    meh

--- backend/db.js
+++ backend/db.js
@@ -45,15 +45,15 @@ module.exports = client;
 // 		console.log('sucess');
 // 	}
 // });
-var createTableString =
-	'CREATE TABLE report(id serial PRIMARY KEY, date_issued VARCHAR (300) NOT NULL, current_conditions VARCHAR (1000) NOT NULL, avalanche_problem_1 VARCHAR (100) NOT NULL, avalanche_problem_2 VARCHAR (100) NOT NULL, avalanche_problem_3 VARCHAR (100) NOT NULL, avalanche_problem_1_description VARCHAR (2000) NOT NULL, avalanche_problem_2_description VARCHAR (2000) NOT NULL, avalanche_problem_3_description VARCHAR (2000) NOT NULL, bottom_line VARCHAR (500) NOT NULL, overall_danger_rose_image VARCHAR (100) NOT NULL, overall_danger_rating VARCHAR (50) NOT NULL, created_on TIMESTAMP NOT NULL)';
+// var createTableString =
+// 	'CREATE TABLE report(id serial PRIMARY KEY, date_issued VARCHAR (300) NOT NULL, current_conditions VARCHAR (1000) NOT NULL, avalanche_problem_1 VARCHAR (100) NOT NULL, avalanche_problem_2 VARCHAR (100) NOT NULL, avalanche_problem_3 VARCHAR (100) NOT NULL, avalanche_problem_1_description VARCHAR (2000) NOT NULL, avalanche_problem_2_description VARCHAR (2000) NOT NULL, avalanche_problem_3_description VARCHAR (2000) NOT NULL, bottom_line VARCHAR (500) NOT NULL, overall_danger_rose_image VARCHAR (100) NOT NULL, overall_danger_rating VARCHAR (50) NOT NULL, created_on TIMESTAMP NOT NULL)';
 
-pool.query(createTableString, (err, resp) => {
-	if (err) console.log('~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ error in initailize reports'), console.log(err);
-	else {
-		console.log('sucessfully loaded into DB');
-	}
-});
+// pool.query(createTableString, (err, resp) => {
+// 	if (err) console.log('~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ error in initailize reports'), console.log(err);
+// 	else {
+// 		console.log('sucessfully loaded into DB');
+// 	}
+// });
 // date_issued
 // current_conditions
 // avalanche_problem_1
