57a4ac4fb75d421474972091a0b16bde2ba8c113
trevjnels <trevorjohnnels@gmail.com>
2019-09-08 15:43:28 -0600

    deployment... updated maps endpoint

--- backend/server.js
+++ backend/server.js
@@ -37,27 +37,27 @@ app.get('/radar', function(req, res) {
 	);
 });
 
-app.get('/maps', (req, res) => {
-	res.send(`function loadMapScenario() {
-		var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-				/* No need to set credentials if already passed in URL */
-				center: new Microsoft.Maps.Location(40.616332, -111.6844868),
-				zoom: 10});
-		// tile url from Iowa Environmental Mesonet of Iowa State University
-		var urlTemplate = 'https://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/nexrad-n0q-{timestamp}/{zoom}/{x}/{y}.png';
-		var timestamps = ['900913-m50m', '900913-m45m', '900913-m40m', '900913-m35m', '900913-m30m', '900913-m25m', '900913-m20m', '900913-m15m', '900913-m10m', '900913-m05m', '900913'];
-		var tileSources = [];
-		for (var i = 0; i < timestamps.length; i++) {
-				var tileSource = new Microsoft.Maps.TileSource({
-						uriConstructor: urlTemplate.replace('{timestamp}', timestamps[i])
-				});
-				tileSources.push(tileSource);
-		}
-		var animatedLayer = new Microsoft.Maps.AnimatedTileLayer({ mercator: tileSources, frameRate: 500 });
-		map.layers.insert(animatedLayer);
-		
-}`);
-}); // helper function to expose the map ojbect to the dom. COuld have had it in the html but wanted to serve from here.
+// app.get('/maps', (req, res) => {
+// 	res.send(`function loadMapScenario() {
+// 		var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+// 				/* No need to set credentials if already passed in URL */
+// 				center: new Microsoft.Maps.Location(40.616332, -111.6844868),
+// 				zoom: 10});
+// 		// tile url from Iowa Environmental Mesonet of Iowa State University
+// 		var urlTemplate = 'https://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/nexrad-n0q-{timestamp}/{zoom}/{x}/{y}.png';
+// 		var timestamps = ['900913-m50m', '900913-m45m', '900913-m40m', '900913-m35m', '900913-m30m', '900913-m25m', '900913-m20m', '900913-m15m', '900913-m10m', '900913-m05m', '900913'];
+// 		var tileSources = [];
+// 		for (var i = 0; i < timestamps.length; i++) {
+// 				var tileSource = new Microsoft.Maps.TileSource({
+// 						uriConstructor: urlTemplate.replace('{timestamp}', timestamps[i])
+// 				});
+// 				tileSources.push(tileSource);
+// 		}
+// 		var animatedLayer = new Microsoft.Maps.AnimatedTileLayer({ mercator: tileSources, frameRate: 500 });
+// 		map.layers.insert(animatedLayer);
+
+// }`);
+// }); // helper function to expose the map ojbect to the dom. COuld have had it in the html but wanted to serve from here.
 
 // if (unix > 1571137200) {
 // 3am on 10/15/2019 next ski season's potential start
